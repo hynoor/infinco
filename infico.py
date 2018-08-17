9e81b70470a1e906c27562f9a78f5a98d7c17790
Deng,Hang <dengh2@emc.com>
2018-08-17 03:55:43 -0400

    Revised

--- infico.py
+++ infico.py
@@ -2,11 +2,45 @@
 import sys
 import paramiko
 import select
+import os
 
 from collections import defaultdict
 
+class Client:
+    """ client host
+    """
+    port = 22
+    def __init__(self, ip=None, user='root', password='Password123!'):
+        """ initialize client host
+        """
+        self.user = user
+        self.password = password
+        self.ip = ip
+        self.jobs = []
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
+        self.client.connect(self.ip, self.port, self.user, self.password)
 
-class InfinCo:
+    def equip(self, target=None ):
+        """ deploy the tool to be executed on the client
+        """
+        sftp = self.client.open_sftp()
+        sftp.put(target, '/tmp/' + os.path.basename(target))
+        sftp.close()
+    
+    def run(self, command):
+        """ execute given command sychronizely 
+        """
+        return
+
+    def run_async(self, command):
+        """ execute given command asychronizely 
+        """
+        return
+    
+
+class Commander:
     """ Collaborative version of Infinio w/ parallelism embeded
     """
     output_size = 1024    
@@ -16,65 +50,64 @@ class InfinCo:
     active_jobs = []
     complete_jobs = []
     status = defaultdict()
-    def __init__(self, partner_info={}, num_job=1):
+    def __init__(self, partner_info=dict(), target=None, num_job=1):
         self.num_job = num_job
-        for pn, pc in partner_info.iterms():
-            pu, pp = pc.split(' ')
-            self.partners.append[(pn, pu, pp )]
+        for pn, pc in partner_info.items():
+            pu, pp = pc.split(':', 2)
+            self.partners.append((pn, pu, pp))
+            self.target = target
 
-    def connect(self)
+    def connect(self):
         """ connect the partner host(s)
         """
         for p in self.partners:
             clnt = paramiko.SSHClient()
-            clnt.load_system_host_keys(()
+            clnt.load_system_host_keys()
             clnt.set_missing_host_key_policy(paramiko.MissingHostKeyPolicy())
             clnt.connect(p[0], 22, p[1], p[2])
-            ssh_clnts.append(clnt)
+            self.ssh_clnts.append(clnt)
 
     def deploy(self):
         """ transfer and deploy infinio.py to partner host(s)
         """
-        for c in ssh_clnts:
-            sftp = c.open_sftp()
-            sftp.put('./infinio.py', '/tmp/infinio.py')
+        for clnt in self.ssh_clnts:
+            sftp = clnt.open_sftp()
+            sftp.put('./' + self.target, '/tmp/' + self.target)
             sftp.close()
 
     def open(self):
         """ open sessions for subsequent execution
         """
-        for clnt in ssh_clnts:
-            session = clnt.get_transport.open_session()
-            channels.append(session)
+        for clnt in self.ssh_clnts:
+            session = clnt.get_transport().open_session()
+            self.channels.append(session)
+        print("Number of Channels: {}".format(len(self.channels)))
 
     def run(self, **kwargs):
         """ start to execute infinio across partners
         """
-        parameters = ''
-        foreach k, v in kwargs:
+        parameters = [self.target]
+        for k, v in kwargs.items():
             parameters.append(k + '=' + v)
-        command = parameters.join(' ')
-
-        for chn in channels:
+        command = ' '.join(parameters)
+        print("command to execute: {}".format(command))
+        for chn in self.channels:
             chn.exec_command(command)
-            self.active_jobs.add(channel)
+            self.active_jobs.append(chn)
 
     def wait(self, timeout):
         """ wait for job done
         """
         for chn in self.active_jobs:
-            while not chn.recv_exit_status():
+            print("Number of active jobs: {}".format(len(self.active_jobs)))
+            while not chn.exit_status():
                 if chn.recv_ready():
-                    status[chn] = 'stdout'
+                    self.status[chn.get_id()] = 'stdout'
                 elif chn.recv_stderr_ready():
-                    status[chn] = 'stderr'
-
-            r, w, x = select(
-                        [job for job in status.keys() if status[job] == 'stdout'],
-                        [], # no stdin
-                        [job for job in status.keys() if status[job] == 'stderr'],
-                        3,  # 3sec timeout
-                    )
+                    self.status[chn.get_id()] = 'stderr'
+            stdout_fhs = [job for job in self.active_jobs if self.status[job.get_id()] == 'stdout'],
+            stderr_fhs = [job for job in self.active_jobs if self.status[job.get_id()] == 'stderr'],
+            r, w, x = select.select(stdout_fhs, [], stderr_fhs, 3)
             if len(r) > 0:
                 output = r.recv(self._output_size)
                 if output != '':
@@ -88,16 +121,29 @@ class InfinCo:
 def test(**kwargs):
     """ test 
     """
+    """
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 
+                "h:", 
+                [
+                    "help",
+                    "action=",
+                    "directory=",
+                ]
+    except getopt.GetoptError as e:
+        # print help information and exit:
+        sys.exit("\n[LOOK HERE]: %s \n\nPlease check helper:\npython infinio.py --help" % str(e))
+    """
     partners = {
-        'localhost': "root:hynoor",
+        'localhost': "root:Password123!",
         }
 
-    infinco = InfinCo(partners)
+    infinco = InfinCo(partner_info=partners, target='command.py')
     infinco.connect()
     infinco.deploy()
-    infinco.run(kwargs)
-    infinco.wait()
-
+    infinco.open()
+    infinco.run()
+    infinco.wait(1000)
 
 
 if __name__ == '__main__':
